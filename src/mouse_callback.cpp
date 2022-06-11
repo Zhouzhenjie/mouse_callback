799b0f7f20d94736f0cd301294bdbeb66c661f89
Jiachen Shen <1836914000@qq.com>
2022-06-11 23:48:05 +0800

    Fix some bugs.

--- src/mouse_callback.cpp
+++ src/mouse_callback.cpp
@@ -4,57 +4,53 @@
 
 #include "mouse_callback/mouse_callback.h"
 
-void MouseCB::onMouse(int event, int x, int y, int flags, void *param)
+void MouseCB::onMouse(int event, int x, int y, int __attribute__((unused)) flags, void* param)
 {
+  auto* p_this = ((MouseCB*)param);
+  static int count = 1;
   if (event == cv::EVENT_LBUTTONDOWN)
   {
-    if (((MouseCB*)param)->points_2d_.size() == 4)
-    {
-      using namespace std;
-      cout << ((MouseCB*)param)->points_2d_[0] << endl;
-      cout << ((MouseCB*)param)->points_2d_[1] << endl;
-      cout << ((MouseCB*)param)->points_2d_[2] << endl;
-      cout << ((MouseCB*)param)->points_2d_[3] << endl;
+    std::cout << "Point" << std::to_string(count++) << ": " << cv::Point2d(x, y) << std::endl;
+    p_this->points_2d_.emplace_back(cv::Point2d(x, y));
+    if (p_this->points_2d_.size() < 4)
+      return;
 
-      cv::Mat_<double> r_vec(3, 1);
-      cv::Mat_<double> t_vec(3, 1);
+    std::cout << "===========================\n";
+    cv::Mat_<double> r_vec(3, 1);
+    cv::Mat_<double> t_vec(3, 1);
 
-      ((MouseCB*)param)->points_3d_.emplace_back(cv::Point3d(-50, -50, 0));
-      ((MouseCB*)param)->points_3d_.emplace_back(cv::Point3d(-50, 50, 0));
-      ((MouseCB*)param)->points_3d_.emplace_back(cv::Point3d(50, 50, 0));
-      ((MouseCB*)param)->points_3d_.emplace_back(cv::Point3d(50, -50, 0));
+    p_this->points_3d_.emplace_back(cv::Point3d(-50, -50, 0));
+    p_this->points_3d_.emplace_back(cv::Point3d(-50, 50, 0));
+    p_this->points_3d_.emplace_back(cv::Point3d(50, 50, 0));
+    p_this->points_3d_.emplace_back(cv::Point3d(50, -50, 0));
 
-      cv::solvePnP(((MouseCB*)param)->points_3d_, ((MouseCB*)param)->points_2d_,
-                   ((MouseCB*)param)->cam_intrinsic_mat_k_, ((MouseCB*)param)->dist_coefficients_,
-                   r_vec, t_vec, false, cv::SOLVEPNP_AP3P);
+    cv::solvePnP(p_this->points_3d_, p_this->points_2d_, p_this->cam_intrinsic_mat_k_, p_this->dist_coefficients_,
+                 r_vec, t_vec, false, cv::SOLVEPNP_AP3P);
 
-      cv::Rodrigues(r_vec, r_vec);
+    cv::Rodrigues(r_vec, r_vec);
 
-      cv::Mat trans;
-      cv::hconcat(r_vec, t_vec, trans);
-      cout << trans << endl;
-      cv::destroyAllWindows();
-    }
-    ((MouseCB*)param)->points_2d_.emplace_back(cv::Point2d(x, y));
+    cv::Mat trans;
+    cv::hconcat(r_vec, t_vec, trans);
+    std::cout << trans << std::endl;
+    cv::destroyAllWindows();
   }
 }
 
-MouseCB::MouseCB(ros::NodeHandle &nh)
+MouseCB::MouseCB(ros::NodeHandle& nh)
 {
   initialize(nh);
   execute();
 }
 
-void MouseCB::initialize(ros::NodeHandle &nh)
+void MouseCB::initialize(ros::NodeHandle& nh)
 {
-  cam_intrinsic_mat_k_ = cv::Mat_<double>(3, 3) << 1265.55389, 0, 666.65908,
-                                                              0, 1266.73808, 526.60665,
-                                                              0, 0, 1;
-  dist_coefficients_ = std::vector<double>{-0.214808, 0.127733, 0.000432, -0.000236, 0.000000};
-  parent_nh_ = nh;
-  ros::NodeHandle input_img_nh(parent_nh_, "input_img");
-  std::string img = this->template getParam<std::string>(input_img_nh, "img_path", "/");
+  // TODO
+  cam_intrinsic_mat_k_ = (cv::Mat_<double>(3, 3) << 1265.55389, 0, 666.65908, 0, 1266.73808, 526.60665, 0, 0, 1);
+  dist_coefficients_ = std::vector<double>{ -0.214808, 0.127733, 0.000432, -0.000236, 0.000000 };
+  std::string img = this->template getParam<std::string>(nh, "img_path", "");
   img_ = cv::imread(img);
+  points_3d_.clear();
+  points_2d_.clear();
   ROS_INFO("Success.");
 }
 
@@ -63,5 +59,8 @@ void MouseCB::execute()
   cv::namedWindow("input_img");
   cv::setMouseCallback("input_img", MouseCB::onMouse, (void*)this);
   cv::imshow("input_img", img_);
-  cv::waitKey(0);
+  while (ros::ok())
+  {
+    cv::waitKey(1);
+  }
 }
